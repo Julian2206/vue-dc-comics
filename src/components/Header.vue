2d7300e2295af92648b2d42cf8c6bedc4a4e5963
Julian2206 <julian.pcc991@gmail.com>
2022-07-15 16:15:35 +0200

    added-vue-header-main-footer

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header id="header">
+    <div class="main-logo">
+      <img src="../assets/img/dc-logo.png" alt="DC Logo" />
+    </div>
+    <nav class="header-nav">
+      <ul>
+        <li>Characters</li>
+        <li>Comics</li>
+        <li>Movies</li>
+        <li>Tv</li>
+        <li>Games</li>
+        <li>Collectable</li>
+        <li>Video</li>
+        <li>Fans</li>
+        <li>News</li>
+        <li>Shop</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
